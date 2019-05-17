b8ebae2c6fd959038cabf351940367eb1d37c38d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-05-16 20:51:39 -0700

    beginning of 3 ryan project?

--- app.js
+++ app.js
@@ -20,22 +20,46 @@ function init() {
   raycaster = new THREE.Raycaster();
   mouse = new THREE.Vector2();
 
-  const geometry = new THREE.SphereGeometry( 1, 20, 20 );
-  const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
-  // const mesh = new THREE.Mesh(geometry, material);
+  let geometry = new THREE.SphereGeometry( 1, 20, 20 );
+  const material = new THREE.MeshLambertMaterial({ 
+    color: 0xFFFFFF
+   });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.scale.set(1.02, 1.0, 1.0);
+  scene.add(mesh);
 
-  // scene.add(mesh);
+  const geometry2 = new THREE.SphereGeometry( 0.25, 20, 20 );
+  const material2 = new THREE.MeshLambertMaterial({ 
+    color: 0xFFFFFF
+   });
+  let mesh2 = new THREE.Mesh(geometry2, material2);
+  mesh2.position.x = 0.6;
+  mesh2.position.y = 0.7;
+  mesh2.position.z = 0.25;
+  scene.add(mesh2);
 
-  for (let i = 0; i < 15; i++) {
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 10;
-    mesh.position.y = (Math.random() - 0.5) * 10;
-    mesh.position.z = (Math.random() - 0.5) * 10;
-    scene.add(mesh);
-  }
+  const geometry3 = new THREE.SphereGeometry( 0.25, 20, 20 );
+  const material3 = new THREE.MeshLambertMaterial({ 
+    color: 0xFFFFFF
+   });
+  let mesh3 = new THREE.Mesh(geometry3, material3);
+  mesh3.position.x = -0.6;
+  mesh3.position.y = 0.7;
+  mesh3.position.z = 0.25;
 
-  light = new THREE.PointLight( 0xff0000, 1, 1000 );
-  light.position.set( 50, 50, 50 );
+  scene.add(mesh2);
+  scene.add(mesh3);
+
+  // for (let i = 0; i < 15; i++) {
+  //   mesh = new THREE.Mesh(geometry, material);
+  //   mesh.position.x = (Math.random() - 0.5) * 10;
+  //   mesh.position.y = (Math.random() - 0.5) * 10;
+  //   mesh.position.z = (Math.random() - 0.5) * 10;
+  //   scene.add(mesh);
+  // }
+
+  light = new THREE.DirectionalLight( 0xFF9900, 1 );
+  light.position.set( 25, 25, 50 );
   scene.add( light );
 
   // light = new THREE.PointLight(0xFFFFFF, 2, 1000)
@@ -89,4 +113,4 @@ init();
 animate();
 
 window.addEventListener("resize", onWindowResize);
-window.addEventListener("click", onMouseMove);
+// window.addEventListener("click", onMouseMove);
